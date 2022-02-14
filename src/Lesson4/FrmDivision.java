01609796d71e1ec1f8f12808955c22f89548b405
Catarina <cf.burghi@gmail.com>
2022-02-14 09:35:59 -0500

    Lesson 4

---
+++ src/Lesson4/FrmDivision.java
@@ -0,0 +1,180 @@
+package Lesson4;
+/*Catarina Burghi
+ * Date: February 14th 2022
+ * This program simulates a calculator for the 
+ * 2 different types of division.
+ */
+
+import java.awt.BorderLayout;
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.border.EmptyBorder;
+import javax.swing.JLabel;
+import javax.swing.GroupLayout;
+import javax.swing.GroupLayout.Alignment;
+import java.awt.Font;
+import javax.swing.JTextField;
+import javax.swing.LayoutStyle.ComponentPlacement;
+import javax.swing.JButton;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
+
+public class FrmDivision extends JFrame {
+
+	private JPanel contentPane;
+	private JTextField txtInsert1;
+	private JTextField txtInsert2;
+	private JTextField txtOutput1;
+	private JTextField txtOutput2;
+	private JTextField txtOutput3;
+	private JTextField txtOutput4;
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					FrmDivision frame = new FrmDivision();
+					frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the frame.
+	 */
+	public FrmDivision() {
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setBounds(100, 100, 450, 300);
+		contentPane = new JPanel();
+		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+		setContentPane(contentPane);
+		
+		JLabel lblInteger = new JLabel("Insert an Integer:");
+		lblInteger.setFont(new Font("Tahoma", Font.BOLD | Font.ITALIC, 11));
+		
+		JLabel lblTitle = new JLabel("Two Types of Division");
+		lblTitle.setFont(new Font("Tahoma", Font.BOLD, 11));
+		
+		JLabel lblSecondInteger = new JLabel("Enter another Integer:");
+		lblSecondInteger.setFont(new Font("Tahoma", Font.BOLD | Font.ITALIC, 11));
+		
+		txtInsert1 = new JTextField();
+		txtInsert1.setColumns(10);
+		
+		txtInsert2 = new JTextField();
+		txtInsert2.setColumns(10);
+		
+		txtOutput1 = new JTextField();
+		txtOutput1.setColumns(10);
+		
+		txtOutput2 = new JTextField();
+		txtOutput2.setColumns(10);
+		
+		txtOutput3 = new JTextField();
+		txtOutput3.setColumns(10);
+		
+		txtOutput4 = new JTextField();
+		txtOutput4.setColumns(10);
+		
+		JButton btnCalculate = new JButton("Calculate");
+		btnCalculate.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				
+				//declaring variables
+				String text1, text2;
+				int num1, num2;
+				
+				//get user input from text boxes
+				text1 = txtInsert1.getText();
+				text2 = txtInsert2.getText();
+				
+				//convert text from text boxes to integers
+				num1 = Integer.parseInt(text1);
+				num2 = Integer.parseInt(text2);
+				
+				//output calculated values to the defined text boxes
+				txtOutput1.setText(num1 + "/" + num2 + "=" + num1/num2);
+				txtOutput2.setText(num1 + "%" + num2 + "=" + num1%num2);
+				txtOutput3.setText(num2 + "/" + num1 + "=" + num2/num1);
+				txtOutput4.setText(num2 + "%" + num1 + "=" + num2%num1);
+			}
+		});
+		
+		JButton btnExit = new JButton("Exit");
+		btnExit.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				
+				//exit
+				System.exit(0);
+			}
+		});
+		
+		GroupLayout gl_contentPane = new GroupLayout(contentPane);
+		gl_contentPane.setHorizontalGroup(
+			gl_contentPane.createParallelGroup(Alignment.LEADING)
+				.addGroup(gl_contentPane.createSequentialGroup()
+					.addGroup(gl_contentPane.createParallelGroup(Alignment.LEADING)
+						.addGroup(gl_contentPane.createSequentialGroup()
+							.addGap(145)
+							.addComponent(lblTitle))
+						.addGroup(gl_contentPane.createSequentialGroup()
+							.addGroup(gl_contentPane.createParallelGroup(Alignment.LEADING)
+								.addGroup(gl_contentPane.createSequentialGroup()
+									.addGap(32)
+									.addGroup(gl_contentPane.createParallelGroup(Alignment.LEADING)
+										.addComponent(txtOutput3, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE)
+										.addComponent(txtOutput1, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE)))
+								.addGroup(gl_contentPane.createSequentialGroup()
+									.addGap(19)
+									.addGroup(gl_contentPane.createParallelGroup(Alignment.LEADING)
+										.addComponent(lblSecondInteger)
+										.addComponent(lblInteger, GroupLayout.PREFERRED_SIZE, 118, GroupLayout.PREFERRED_SIZE))))
+							.addGap(21)
+							.addGroup(gl_contentPane.createParallelGroup(Alignment.LEADING)
+								.addGroup(gl_contentPane.createSequentialGroup()
+									.addGroup(gl_contentPane.createParallelGroup(Alignment.LEADING)
+										.addComponent(txtInsert1, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE)
+										.addComponent(txtInsert2, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE))
+									.addGap(50)
+									.addGroup(gl_contentPane.createParallelGroup(Alignment.TRAILING)
+										.addComponent(btnCalculate)
+										.addComponent(btnExit)))
+								.addComponent(txtOutput2, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE)
+								.addComponent(txtOutput4, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE))))
+					.addContainerGap(GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
+		);
+		gl_contentPane.setVerticalGroup(
+			gl_contentPane.createParallelGroup(Alignment.LEADING)
+				.addGroup(gl_contentPane.createSequentialGroup()
+					.addComponent(lblTitle)
+					.addGap(33)
+					.addGroup(gl_contentPane.createParallelGroup(Alignment.BASELINE)
+						.addComponent(lblInteger, GroupLayout.PREFERRED_SIZE, 29, GroupLayout.PREFERRED_SIZE)
+						.addComponent(txtInsert1, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE)
+						.addComponent(btnCalculate))
+					.addGap(18)
+					.addGroup(gl_contentPane.createParallelGroup(Alignment.BASELINE)
+						.addComponent(lblSecondInteger)
+						.addComponent(txtInsert2, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE)
+						.addComponent(btnExit))
+					.addGap(34)
+					.addGroup(gl_contentPane.createParallelGroup(Alignment.BASELINE)
+						.addComponent(txtOutput1, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE)
+						.addComponent(txtOutput2, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE))
+					.addGap(18)
+					.addGroup(gl_contentPane.createParallelGroup(Alignment.BASELINE)
+						.addComponent(txtOutput4, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE)
+						.addComponent(txtOutput3, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE))
+					.addContainerGap(42, Short.MAX_VALUE))
+		);
+		contentPane.setLayout(gl_contentPane);
+	}
+}
