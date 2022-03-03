255d5b0e90991e4a690edd66f0c3bbb0c2dc9f6f
Catarina <cf.burghi@gmail.com>
2022-03-03 08:58:28 -0500

    Lesson 5

--- src/Lesson4/FrmSleepCalculator.java
+++ src/Lesson4/FrmSleepCalculator.java
@@ -14,7 +14,6 @@ import javax.swing.border.EmptyBorder;
 import java.awt.Color;
 import java.awt.CardLayout;
 import javax.swing.JLabel;
-import net.miginfocom.swing.MigLayout;
 import java.awt.Font;
 import javax.swing.JTextField;
 import javax.swing.JButton;
---
+++ src/Lesson5/WhileLoopsPractice.java
@@ -0,0 +1,14 @@
+package Lesson5;
+
+public class WhileLoopsPractice {
+    
+    public static void main(String[] args) {
+        System.out.println("Counting down:");
+
+        int i = 6;
+        while (i > 0) {
+            System.out.println(i);
+            i--;
+        }
+    }
+}
